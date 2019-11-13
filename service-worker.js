f49f959b8dc7768b2d1520ef35cd160abaae8850
suzuki kentaro <yqvmmifnbdabfb412tzb@gmail.com>
2019-11-13 23:55:21 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/suzukik1992/suzukik1992.github.io/precache-manifest.c14fd6385fadc190ae340068aa6621b7.js"
+  "/precache-manifest.12b329b4dc17980a54ea5b8ca51973f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/suzukik1992/suzukik1992.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
