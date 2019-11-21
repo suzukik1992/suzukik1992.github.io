07ad4a4b06d60fb2ac75e0b7123317345c4eb356
suzuki kentaro <yqvmmifnbdabfb412tzb@gmail.com>
2019-11-22 03:16:02 +0900

    changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Home } from './home'; 
 import { WorkPages　} from './workPages';
 import { About } from './about';
