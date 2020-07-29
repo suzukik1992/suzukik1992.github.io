853ddc75d857a5ce60461e83ba556d35f649e1ff
suzuki kentaro <yqvmmifnbdabfb412tzb@gmail.com>
2020-07-29 10:14:31 +0900

    Updates

---
+++ precache-manifest.70a4f4965746a237aeb890e7499896ea.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5191d9d404de5ea9c06dc19e220dfc1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "821e16b9815c9d9acda3",
+    "url": "/static/css/main.39df418a.chunk.css"
+  },
+  {
+    "revision": "e9ddb621b3510d72a541",
+    "url": "/static/js/2.ff332683.chunk.js"
+  },
+  {
+    "revision": "821e16b9815c9d9acda3",
+    "url": "/static/js/main.f37ad77b.chunk.js"
+  },
+  {
+    "revision": "3d6a54f55701d39d1437",
+    "url": "/static/js/runtime-main.f6aec8aa.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/static/media/Montserrat-Thin.43dd5b7a.ttf"
+  },
+  {
+    "revision": "3acceeae16c7fe1e609459f19d4c3e03",
+    "url": "/static/media/amcj.3acceeae.jpg"
+  },
+  {
+    "revision": "c48bd661d316b3b672f92cfc21f19120",
+    "url": "/static/media/amcj_002.c48bd661.jpg"
+  },
+  {
+    "revision": "a50c6f7a99e775dd05acadea197a77b6",
+    "url": "/static/media/amcj_002_works.a50c6f7a.jpg"
+  },
+  {
+    "revision": "872259c76a080b97d908b46389ec0f99",
+    "url": "/static/media/amcj_fes2018.872259c7.jpg"
+  },
+  {
+    "revision": "d1bf8b38b7266fd3e6b7c5056eb592b9",
+    "url": "/static/media/ball_nagoya.d1bf8b38.jpg"
+  },
+  {
+    "revision": "910ea7ded007626ad1f565ec233fee7a",
+    "url": "/static/media/ball_nagoya_work_1.910ea7de.jpg"
+  },
+  {
+    "revision": "084dde4ed75789ce7c611726b74ecd0e",
+    "url": "/static/media/bouldering.084dde4e.jpg"
+  },
+  {
+    "revision": "33a384341a8f92847e441143a08ae860",
+    "url": "/static/media/bouldering_work_1.33a38434.jpg"
+  },
+  {
+    "revision": "1c540343ca3f32847bfd2a4c15a7e514",
+    "url": "/static/media/bouldering_work_2.1c540343.jpg"
+  },
+  {
+    "revision": "e3d7d2e70cfb3431e65d957c08237d76",
+    "url": "/static/media/cristalUniverse.e3d7d2e7.jpg"
+  },
+  {
+    "revision": "e84e5325a66e31df0559a7df1bd38c12",
+    "url": "/static/media/cristalUniverse_work_1.e84e5325.jpg"
+  },
+  {
+    "revision": "cdf60a326cae3dd1b6ee68e9071418a9",
+    "url": "/static/media/cristalUniverse_work_2.cdf60a32.jpg"
+  },
+  {
+    "revision": "81ec1cc20096edb934e409b4f75fb6b3",
+    "url": "/static/media/cristalUniverse_work_3.81ec1cc2.jpg"
+  },
+  {
+    "revision": "5bc06ba392b323b13c7fce5c04e10115",
+    "url": "/static/media/cristalWorld.5bc06ba3.jpg"
+  },
+  {
+    "revision": "76996dce6d4ceb963fbb012ac4bf235d",
+    "url": "/static/media/cristalWorld_work_1.76996dce.jpg"
+  },
+  {
+    "revision": "0cc67eae853b49c4e72510f4c34d36ec",
+    "url": "/static/media/cristalWorld_work_2.0cc67eae.jpg"
+  },
+  {
+    "revision": "dd52727a56f2b919bedeb2b74b652d15",
+    "url": "/static/media/cristalWorld_work_3.dd52727a.jpg"
+  },
+  {
+    "revision": "859530cf7badb7c32f054734ad13347d",
+    "url": "/static/media/evessa.859530cf.jpg"
+  },
+  {
+    "revision": "82995613ccc67152360c22e33b23aac7",
+    "url": "/static/media/floatingLamp.82995613.jpg"
+  },
+  {
+    "revision": "5861e1be6f39a0e6466e146c8bbb3aca",
+    "url": "/static/media/floatingLamp_work_1.5861e1be.jpg"
+  },
+  {
+    "revision": "372f6610e1c9449cc429627aa35ea73d",
+    "url": "/static/media/floatingLamp_work_2.372f6610.jpg"
+  },
+  {
+    "revision": "14335816f1804e814fd002107bf589f7",
+    "url": "/static/media/kintowari.14335816.jpg"
+  },
+  {
+    "revision": "7e1dff9ec780be777a7a5a04327dffe0",
+    "url": "/static/media/kintowari_work_1.7e1dff9e.jpg"
+  },
+  {
+    "revision": "c87af0ff56b014158b1f67c2a17c00ed",
+    "url": "/static/media/loopSet.c87af0ff.jpg"
+  },
+  {
+    "revision": "9a0f1fb7987269f54f77cd64aa79c064",
+    "url": "/static/media/mangrove.9a0f1fb7.jpg"
+  },
+  {
+    "revision": "1a981743f4ec774d10fd5b8a1129ce8e",
+    "url": "/static/media/mangrove_work_1.1a981743.jpg"
+  },
+  {
+    "revision": "1c36ea71657825d6d507a7174296bbc3",
+    "url": "/static/media/mangrove_work_2.1c36ea71.jpg"
+  },
+  {
+    "revision": "2a3b6ada80884e98f9460a8981d7a992",
+    "url": "/static/media/mangrove_work_3.2a3b6ada.jpg"
+  },
+  {
+    "revision": "2b53faa694742d82edb42af00294d9aa",
+    "url": "/static/media/me.2b53faa6.jpg"
+  },
+  {
+    "revision": "a58a648695f89df7fa4c0af2fdb28d9c",
+    "url": "/static/media/memory.a58a6486.jpg"
+  },
+  {
+    "revision": "a45289fef87affb5a7e1d94f8027d2fd",
+    "url": "/static/media/memory_work_1.a45289fe.jpg"
+  },
+  {
+    "revision": "1ead617fde015f01777427df2cc5505b",
+    "url": "/static/media/memory_work_2.1ead617f.jpg"
+  },
+  {
+    "revision": "06e9803c9789c60da7b4c33bdca99d92",
+    "url": "/static/media/memory_work_3.06e9803c.jpg"
+  },
+  {
+    "revision": "52317ea6e62bfc2a39d792e896198d4d",
+    "url": "/static/media/monolith.52317ea6.jpg"
+  },
+  {
+    "revision": "018df694bb79a3e68573de5ac57883c5",
+    "url": "/static/media/monolith_work_1.018df694.jpg"
+  },
+  {
+    "revision": "73987572940a48d9f7e093a30d4d8f73",
+    "url": "/static/media/monolith_work_2.73987572.jpg"
+  },
+  {
+    "revision": "ceaa7631a3aec88059232fbe496404b1",
+    "url": "/static/media/monsterHunterWorld.ceaa7631.jpg"
+  },
+  {
+    "revision": "7f35549f3c58e0f840ec3a3513768eed",
+    "url": "/static/media/monsterHunterWorld_work_main.7f35549f.jpg"
+  },
+  {
+    "revision": "c79423cfaa3e9f986899f5859c715f54",
+    "url": "/static/media/peace.c79423cf.jpg"
+  },
+  {
+    "revision": "0dc7fbe3dd30addb206d3d5943148a23",
+    "url": "/static/media/peace_work_1.0dc7fbe3.jpg"
+  },
+  {
+    "revision": "586e88d2f1f0bdf1167dc299b87187d6",
+    "url": "/static/media/peace_work_2.586e88d2.jpg"
+  },
+  {
+    "revision": "1d7b59a45e27963ffc64b6614cd3bda0",
+    "url": "/static/media/prosound_201906.1d7b59a4.jpg"
+  },
+  {
+    "revision": "99dac9cb309ff2dc80de51283a3498c6",
+    "url": "/static/media/prosound_201906_work_1.99dac9cb.jpg"
+  },
+  {
+    "revision": "f76468d4e1433d037d6bd7ebd7782c65",
+    "url": "/static/media/soundAndRecording_202001.f76468d4.jpg"
+  },
+  {
+    "revision": "8856d7fd7066e0815e764ede0c7a3d1d",
+    "url": "/static/media/soundAndRecording_202001_title.8856d7fd.jpg"
+  },
+  {
+    "revision": "9d9e8321572eb27d53c6a5ecdafb6fc7",
+    "url": "/static/media/sourceCord_002.9d9e8321.jpg"
+  },
+  {
+    "revision": "e644b2478e7bf445e2072e35b1c752bd",
+    "url": "/static/media/synesthesiaLab.e644b247.jpg"
+  },
+  {
+    "revision": "0cc606818fdc0c700d38f167d099e823",
+    "url": "/static/media/synesthesiaLab_work_1.0cc60681.jpg"
+  },
+  {
+    "revision": "5329939bc63e81d5e75188eb99420c8b",
+    "url": "/static/media/world.5329939b.jpg"
+  },
+  {
+    "revision": "2451888cd860d26751d48fbdc949b91f",
+    "url": "/static/media/world_work_1.2451888c.jpg"
+  },
+  {
+    "revision": "2055f88edb77db3c77319e83168475ca",
+    "url": "/static/media/world_work_2.2055f88e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/suzuki-kentaro/precache-manifest.988c6f6c4cb691e01317c6896a96bc59.js"
+  "/precache-manifest.70a4f4965746a237aeb890e7499896ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/suzuki-kentaro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
